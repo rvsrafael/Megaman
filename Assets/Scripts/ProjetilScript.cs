d643c98685b1c1f5be9f57d2354cf2274f64d654
logonrm <logonrm@fiap.com.br>
2018-02-17 11:46:48 -0200

    first commit

---
+++ Assets/Scripts/ProjetilScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilScript : MonoBehaviour {
+
+    public float velocidade;
+    
+	void Start () {
+        // Destroi caso o projetil nao acerte nada
+        Destroy(gameObject, 3.0f);	
+	}
+
+    void OnCollisionEnter2D(Collision2D c){
+        // Destroi o projetil por colisao
+        Destroy(gameObject);
+    }
+
+    void Update () {
+        // Mover o projetil
+        transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+	}
+}
